801925fff97eb7f1300b4ebebf05976514e1ecdd
Ed Shadi <edshadi@gmail.com>
2013-06-25 09:43:35 -0700

    initial commit with rspec, capybara, shoulda and factory_girl

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TddTodoRails::Application.config.secret_token = 'b7b3e1508393367efa47baa35043d5c81acc9b5b6c647b620307d289c68b490530ef00ea4672aa322bf951b305819abcb50facea7344014ba3dc5cf58641b707'
